86debbf67409fc338849b2b9e141eeda46b2a0e9
nicky0419@daum.net <matricks1924>
2020-02-03 15:26:54 +0900

    저작권 명세 다시 추가

--- ByulSoft/js/script.js
+++ ByulSoft/js/script.js
@@ -408,3 +408,54 @@ timelines.forEach(val => {
     );
 });
 
+//licence data
+const licences = [
+    {
+        'licenceTitle': 'Slide Images',
+        'licenceText':
+        '<li><a href="https://www.freepik.com/free-photos-vectors/icon">Icon vector created by fullvector - www.freepik.com</a></li>'
+    },
+    {
+        'licenceTitle': 'Icons',
+        'licenceText':
+        '<li>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></li>' +
+        '<li><a href="https://material.io/resources/icons/?style=baseline">material.io</a></li>'
+    },
+    {
+        'licenceTitle': 'Layout',
+        'licenceText':
+        '<li>https://getbootstrap.com/</li>' +
+        '<li>https://getuikit.com/</li>'
+    },
+    {
+        'licenceTitle': 'Scrollbar',
+        'licenceText':
+        '<li>http://manos.malihu.gr/jquery-custom-content-scroller/</li>'
+    },
+    {
+        'licenceTitle': 'Timeline',
+        'licenceText':
+        '<li>' +
+        'Copyright (c) 2020 by Ratko Solaja (https://codepen.io/Ratko_Solaja/pen/ztxkw)<br>' +
+        'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:<br>' +
+        'The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.<br>' +
+        'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.<br>' +
+        '</li>'
+    },
+    {
+        'licenceTitle': 'Background Effect',
+        'licenceText':
+        '<li>' +
+        'Copyright (c) 2019 by Francesco Allegrini (https://codepen.io/francesco-allegrini/pen/jeQpaL)<br>' +
+        'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:<br>' +
+        'The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.<br>' +
+        'HE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.<br>' +
+        '</li>'
+    },
+];
+licences.forEach(val => {
+    $('.licence').append(
+        "<h5>- " + val.licenceTitle + "</h5>" +
+        val.licenceText
+    );
+});
